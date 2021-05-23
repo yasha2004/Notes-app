4922f0efc162397cd3d6b5077c9d25d915c1cca5
yash agarwal <agarwal.yash36@gmail.com>
2021-05-24 02:55:53 +0530

    App Completed

---
+++ nodes-app/app.js
@@ -0,0 +1,63 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes');
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler (argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler (argv) {
+        notes.removeNote(argv.title);
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists your notes',
+    handler () {
+        notes.listNotes();
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler (argv) {
+        notes.readNote(argv.title);
+    }
+});
+
+ console.log(yargs.argv);
